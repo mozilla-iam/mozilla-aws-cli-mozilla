4cf6936b2da55a15906674fdc3f6a26c6c7b6012
Gene Wood <gene_wood@cementhorizon.com>
2019-11-26 16:58:16 -0800

    Add long description

--- setup.py
+++ setup.py
@@ -2,10 +2,14 @@
 
 from setuptools import setup
 
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
 setup(
     name="mozilla_aws_cli_mozilla",
     description="Mozilla specific deployment of the mozilla_aws_cli",
     author="Mozilla Enterprise Information Security",
+    author_email="iam@discourse.mozilla.org",
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
@@ -15,8 +19,10 @@ setup(
     ],
     install_requires=["mozilla_aws_cli"],
     keywords="maws Mozilla AWS CLI config",
+    long_description=readme,
+    long_description_content_type='text/markdown',
     packages=["mozilla_aws_cli_config"],
     url="https://github.com/mozilla-iam/mozilla-aws-cli-mozilla",
-    version="0.0.1",
+    version="1.0.0",
     zip_safe=False,
 )
